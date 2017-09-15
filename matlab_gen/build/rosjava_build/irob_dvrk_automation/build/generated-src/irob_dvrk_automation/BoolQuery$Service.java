32c575e85e27d663490510ffb6a67d9de1cb4df3
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2017-09-15 12:05:31 +0200

    Dissection_actionlib ready to merge with master

--- matlab_gen/build/rosjava_build/irob_dvrk_automation/build/generated-src/irob_dvrk_automation/BoolQuery$Service.java
+++
@@ -1,8 +0,0 @@
-package irob_dvrk_automation;
-
-public interface BoolQuery$Service extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "irob_dvrk_automation/BoolQuery$Service";
-  static final java.lang.String _DEFINITION = "#request fields\n---\n#response fields\nbool data\n";
-  static final boolean _IS_SERVICE = true;
-  static final boolean _IS_ACTION = false;
-}
--- matlab_gen/build/rosjava_build/irob_dvrk_automation/build/generated-src/irob_dvrk_automation/BoolQueryRequest.java
+++
@@ -1,8 +0,0 @@
-package irob_dvrk_automation;
-
-public interface BoolQueryRequest extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "irob_dvrk_automation/BoolQueryRequest";
-  static final java.lang.String _DEFINITION = "#request fields\n";
-  static final boolean _IS_SERVICE = true;
-  static final boolean _IS_ACTION = false;
-}
