e0f3bdf19ac7b5eb1a0e013099c17749129df301
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2018-10-29 14:26:41 +0100

    Publish to VPS works fine

--- irob_robot/scripts/rosbridge_subscribe.py
+++ irob_robot/scripts/rosbridge_subscribe.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import sys
-import rospy
 import time
 
 import roslibpy
@@ -17,39 +16,40 @@ from rospy_message_converter import message_converter
 class ROSbridgeSubscriber():
 
   def __init__(self):
-    ros = roslibpy.Ros(host='localhost', port=9090)
 
-
-    rospy.init_node('rosbridge_subscriber', anonymous=True)
-
-    joint_sub = rospy.Subscriber("/dvrk/PSM1/state_joint_current",JointState,self.joint_cb)
-
-
+    self.ros = roslibpy.Ros(host='localhost', port=9090, is_secure=False)
+    self.ros.on_ready(self.connect_cb)
     topic_name = "/twin/dvrk/PSM1/state_joint_current"
-
-    self.topic = roslibpy.Topic(ros, topic_name, JointState)
+    self.topic = roslibpy.Topic(self.ros, topic_name, 'sensor_msgs/JointState')
     self.topic.advertise()
 
-    #ros.run_forever()
+    self.ros.run_forever()
 
 
   def joint_cb(self, msg):
     #print(msg)
-    dictionary =     message_converter.convert_ros_message_to_dictionary(msg)
+    dictionary = message_converter.convert_ros_message_to_dictionary(msg)
     bridge_msg = roslibpy.Message(dictionary)
     self.topic.publish(bridge_msg)
+    print(msg)
 
+
+
+  def connect_cb(self):
+    print("Is connected?")
+    print(self.ros.is_connected)
+    joint_sub = rospy.Subscriber("/dvrk/PSM1/state_joint_current",JointState,self.joint_cb)
 
 
 
 def main(args):
   print("Node started")
+  rospy.init_node('rosbridge_subscriber', anonymous=True)
+  rospy.Rate(10)
   rbs = ROSbridgeSubscriber()
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
+
 
 
 if __name__ == '__main__':
   main(sys.argv)
+
