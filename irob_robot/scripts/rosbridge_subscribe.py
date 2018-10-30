38cf9530b2bbc7e7a617aedac9ff0c78de9fffa7
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2018-10-30 13:49:01 +0100

    Python and launch files for MaxWhere upstream done

--- irob_robot/scripts/rosbridge_subscribe.py
+++ irob_robot/scripts/rosbridge_subscribe.py
@@ -15,9 +15,9 @@ from rospy_message_converter import message_converter
 
 class ROSbridgeSubscriber():
 
-  def __init__(self):
+  def __init__(self, host, port):
 
-    self.ros = roslibpy.Ros(host='localhost', port=9090, is_secure=False)
+    self.ros = roslibpy.Ros(host=host, port=port, is_secure=False)
     self.ros.on_ready(self.connect_cb)
     topic_name = "/twin/dvrk/PSM1/state_joint_current"
     self.topic = roslibpy.Topic(self.ros, topic_name, 'sensor_msgs/JointState')
@@ -38,7 +38,7 @@ class ROSbridgeSubscriber():
   def connect_cb(self):
     print("Is connected?")
     print(self.ros.is_connected)
-    joint_sub = rospy.Subscriber("/dvrk/PSM1/state_joint_current",JointState,self.joint_cb)
+    rospy.Subscriber("/dvrk/PSM1/state_joint_current",JointState,self.joint_cb)
 
 
 
@@ -46,7 +46,11 @@ def main(args):
   print("Node started")
   rospy.init_node('rosbridge_subscriber', anonymous=True)
   rospy.Rate(10)
-  rbs = ROSbridgeSubscriber()
+
+  host = rospy.get_param('~host')
+  port = rospy.get_param('~port')
+
+  rbs = ROSbridgeSubscriber(host, port)
 
 
 
