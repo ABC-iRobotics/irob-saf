b6eb88619862d7cba4918446e54fac63f8a6bef9
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2018-11-07 10:49:31 +0100

    Few more comments

--- irob_connect/README.md
+++ irob_connect/README.md
@@ -2,6 +2,10 @@
 
 Package for publishing the joint states of the DVRK on a websocket.
 
+To enable execution of Python node, type
+
+    chmod +x <path>/catkin_ws/src/irob_saf/irob_connection/scripts/dvrk_rosbridge_upstream.py
+
 To launch publisher, type
 
     roslaunch irob_connect dvrk_rosbridge_upstream.launch host:=<IP_address_of_host>
--- irob_connect/scripts/robot_arm_psm.py
+++ irob_connect/scripts/robot_arm_psm.py
@@ -12,9 +12,12 @@ from rospy_message_converter import message_converter
 
 from robot_arm_dvrk import RobotArmDVRK
 
-
+"""
+Rosbridge node for PSM-s.
+"""
 class RobotArmPSM(RobotArmDVRK):
 
+  # Mapping between ROS and rosbridge joint names.
   JOINT_DICT = {'outer_yaw': 'yaw_joint',
                 'outer_pitch': 'pitch_back_joint',
                 'outer_insertion': 'main_insertion_joint',
@@ -23,6 +26,7 @@ class RobotArmPSM(RobotArmDVRK):
                 'outer_wrist_yaw': 'tool_yaw_joint',
                 'jaw': 'tool_gripper2_joint'}
 
+  # Array of the rosbridge joint names in an order to be publsihed
   JOINT_NAMES_BRIDGE = ['rev_joint','yaw_joint','pitch_back_joint',
                         'pitch_bottom_joint','pitch_end_joint',
                         'main_insertion_joint', 'tool_roll_joint',
@@ -30,18 +34,28 @@ class RobotArmPSM(RobotArmDVRK):
                         'tool_gripper1_joint', 'tool_gripper2_joint',
                         'pitch_top_joint','pitch_front_joint']
 
+
+
+
+  """Constructor.
+  :param name: name of the arm
+  :param ros: ros object
+  """
   def __init__(self, name, ros):
     RobotArmDVRK.__init__(self, name, ros,
                           self.JOINT_DICT, self.JOINT_NAMES_BRIDGE)
 
 
-
+  """Overrided subscription method. The state_jaw_current topic
+  is needed besides the state_joint_current.
+  """
   def subscribe_to_topics(self):
     RobotArmDVRK.subscribe_to_topics(self)
     rospy.Subscriber("/dvrk/" + self.name + "/state_jaw_current", JointState, self.jaw_cb)
 
 
-
+  """Callback for the jaw position.
+  """
   def jaw_cb(self, msg):
     self.store_joint_states_from_msg(msg)
 
