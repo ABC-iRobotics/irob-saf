92e8580016f0a75cb216a658e1c823d8a6154281
Tamas Levendovics <tamas.levendovics@irob.uni-obuda.hu>
2025-10-13 23:25:48 +0200

    Debug dummy_target_publisher

--- irob_vision_support/launch/dummy_target.launch.py
+++ irob_vision_support/launch/dummy_target.launch.py
@@ -5,30 +5,33 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    ns = LaunchConfiguration('ns')
-    img_topic = LaunchConfiguration('image_topic')
-    target_topic = LaunchConfiguration('target_topic')
-    parent_frame = LaunchConfiguration('parent_frame')
-    child_frame = LaunchConfiguration('child_frame')
+    # Declare configurable arguments
+    frame_id = LaunchConfiguration('frame_id')
+    rate_hz = LaunchConfiguration('rate_hz')
 
     return LaunchDescription([
-        DeclareLaunchArgument('ns', default_value='vision'),
-        DeclareLaunchArgument('image_topic', default_value='/camera/image_preprocessed'),
-        DeclareLaunchArgument('target_topic', default_value='/vision/target'),
-        DeclareLaunchArgument('parent_frame', default_value='camera_frame'),
-        DeclareLaunchArgument('child_frame', default_value='target_frame'),
+        # Launch arguments
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value='PSM1_psm_base_link',
+            description='Parent TF frame for dummy target'
+        ),
+        DeclareLaunchArgument(
+            'rate_hz',
+            default_value='10.0',
+            description='Publishing rate (Hz)'
+        ),
 
+        # Dummy target node
         Node(
             package='irob_vision_support',
-            executable='vision_server_test_dummy_node',
-            namespace=ns,
-            name='dummy_vision_server',
+            executable='dummy_target_publisher_node',
+            name='dummy_target_publisher',
+            namespace='saf',
             output='screen',
             parameters=[{
-                'image_topic': img_topic,
-                'target_topic': target_topic,
-                'parent_frame': parent_frame,
-                'child_frame': child_frame
+                'frame_id': frame_id,
+                'rate_hz': rate_hz,
             }]
-        )
+        ),
     ])
--- irob_vision_support/src/dummy_target_publisher.cpp
+++ irob_vision_support/src/dummy_target_publisher.cpp
@@ -1,22 +1,19 @@
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <visualization_msgs/msg/marker.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <cmath>
 
 class DummyTargetPublisher : public rclcpp::Node {
 public:
     DummyTargetPublisher()
-    : rclcpp::Node("dummy_target_publisher"),
-      tf_broadcaster_(this)
+    : rclcpp::Node("dummy_target_publisher")
     {
-        this->declare_parameter<std::string>("parent_frame", "camera_frame");
-        this->declare_parameter<std::string>("child_frame", "target_frame");
-        this->declare_parameter<double>("rate_hz", 30.0);
-        this->get_parameter("parent_frame", parent_frame_);
-        this->get_parameter("child_frame", child_frame_);
+        this->declare_parameter<std::string>("frame_id", "world");
+        this->declare_parameter<double>("rate_hz", 10.0);
+        this->get_parameter("frame_id", frame_id_);
         this->get_parameter("rate_hz", rate_);
 
-        pub_ = this->create_publisher<geometry_msgs::msg::TransformStamped>("/vision/target", 10);
+        pub_ = this->create_publisher<visualization_msgs::msg::Marker>("/vision/target", 10);
         timer_ = this->create_wall_timer(
           std::chrono::duration<double>(1.0 / rate_),
           std::bind(&DummyTargetPublisher::tick_, this));
@@ -26,31 +23,41 @@ public:
 private:
     void tick_() {
         auto now = this->get_clock()->now();
-        geometry_msgs::msg::TransformStamped t;
-        t.header.stamp = now;
-        t.header.frame_id = parent_frame_;
-        t.child_frame_id = child_frame_;
+        visualization_msgs::msg::Marker marker;
+        marker.header.stamp = now;
+        marker.header.frame_id = frame_id_;
+        marker.ns = "dvrk_viz";
+        marker.id = marker_id_;
+        marker_id_++;
 
-        const double s = std::sin(now.seconds());
-        t.transform.translation.x = 0.10 * s;
-        t.transform.translation.y = 0.00;
-        t.transform.translation.z = 0.40;
-        t.transform.rotation.x = 0.0;
-        t.transform.rotation.y = 0.0;
-        t.transform.rotation.z = 0.0;
-        t.transform.rotation.w = 1.0;
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.pose.position.x = -0.05;
+        marker.pose.position.y = 0.08;
+        marker.pose.position.z = -0.12;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+        marker.scale.x = 0.005;
+        marker.scale.y = 0.005;
+        marker.scale.z = 0.005;
+        marker.color.a = 1.0; // Don't forget to set the alpha!
+        marker.color.r = 0.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
 
-        tf_broadcaster_.sendTransform(t);
-        pub_->publish(t);
+        pub_->publish(marker);
     }
 
-    std::string parent_frame_;
+    std::string frame_id_;
     std::string child_frame_;
     double rate_{30.0};
+    int marker_id_{0};
 
-    rclcpp::Publisher<geometry_msgs::msg::TransformStamped>::SharedPtr pub_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_;
     rclcpp::TimerBase::SharedPtr timer_;
-    tf2_ros::TransformBroadcaster tf_broadcaster_;
+
 };
 
 int main(int argc, char **argv) {
